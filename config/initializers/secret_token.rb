54168dbee912f425181480258e8be6a76a2b75d2
albrow <stephenalexbrowne@gmail.com>
2013-12-27 16:48:39 -0500

    initial commit. barebones rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TheCube::Application.config.secret_key_base = '2053cdd02347eb878a1bba470fe09e6fcaab7b49b048250c510a1be36f08f33a4b6e46f17db5ccd363f27335426c4653652bdaf8abb63433eacd93f3cb86cd7f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TheCube::Application.config.session_store :cookie_store, key: '_the_cube_session'
