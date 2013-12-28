9889d4ba0cb084f81f41ef056718a7e4ecae77e2
albrow <stephenalexbrowne@gmail.com>
2013-12-28 16:40:37 -0500

    Add email hash and profile_image_url method to admin users

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,10 +1,20 @@
 class AdminUser < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, 
+         :recoverable, :rememberable, :trackable, :validatable
 	# Include default devise modules. Others available are:
 	# :confirmable, :lockable, :timeoutable and :omniauthable
 	devise :database_authenticatable, 
 	:recoverable, :rememberable, :trackable, :validatable
 
+	# == callbacks
+	before_save :compile_email_hash
+
 	# == methods
+	def compile_email_hash
+		self.email_hash = Digest::MD5.hexdigest(self.email)
+	end
 
 	# a convenience method for getting the admin users's full name
 	def full_name
@@ -15,5 +25,12 @@ class AdminUser < ActiveRecord::Base
 		else
 			return nil
 		end
+	end
+
+	# returns a url which can be used as the src attribute in an img tag
+	# powered by gravatar
+	# http://en.gravatar.com/site/implement/images/ruby/
+	def profile_image_url(size=400)
+		"http://www.gravatar.com/avatar/#{self.email_hash}?size=#{size}"
 	end
 end
---
+++ db/migrate/20131228213103_add_email_hash_to_admin_user.rb
@@ -0,0 +1,9 @@
+class AddEmailHashToAdminUser < ActiveRecord::Migration
+	def change
+		add_column :admin_users, :email_hash, :string
+		# create email hashes for any admin users that might not have them
+		AdminUser.all.each do |u|
+			u.compile_email_hash
+		end
+	end
+end
